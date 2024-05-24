d0c8f6d36ce2b1757ed2a5f794ff2f7c34053b2c
Svyter <Max161161161@mail.ru>
2024-05-24 17:11:30 +0300

    Edit trainingsModel

--- src/main/java/com/svyter/spring/swimingbysvyter/model/TrainingsModel.java
+++ src/main/java/com/svyter/spring/swimingbysvyter/model/TrainingsModel.java
@@ -1,10 +1,17 @@
 package com.svyter.spring.swimingbysvyter.model;
 
+import com.svyter.spring.swimingbysvyter.dto.TrainingsRepo;
+import com.svyter.spring.swimingbysvyter.entity.Inventory;
 import com.svyter.spring.swimingbysvyter.entity.Trainings;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.NotBlank;
+import java.util.List;
+
 @Data
+@Component
 public class TrainingsModel {
     @NotBlank
     private String name;
@@ -14,21 +21,27 @@ public class TrainingsModel {
     private String mainTraining;
     @NotBlank
     private String hitch;
+/*    private List<String> inventoryList;*/
+  /*  @Autowired
+    private InventoryRepo inventoryRepo;*/
 
     public TrainingsModel() {
     }
 
-    public TrainingsModel(String name, String warmUp, String mainTraining, String hitch) {
+    public TrainingsModel(String name, String warmUp, String mainTraining, String hitch/*, List<String> inventoryList*/) {
         this.name = name;
         this.warmUp = warmUp;
         this.mainTraining = mainTraining;
         this.hitch = hitch;
+        /*this.inventoryList = inventoryList;*/
     }
 
     public static TrainingsModel convertToModel(Trainings trainings)
     {
         TrainingsModel trainingsModel = new TrainingsModel(trainings.getName(),trainings.getWarmUp(),
-                                                            trainings.getMainTraining(), trainings.getHitch());
+                                                            trainings.getMainTraining(), trainings.getHitch()
+                                                          /*  ,trainings.getInventoryList().stream().map()        */
+                );
         return trainingsModel;
     }
 }
